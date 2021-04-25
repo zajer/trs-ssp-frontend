4c56e83a2e2e23c9b38e66a7db5aab7585026f43
zajer <pikus3@list.pl>
2021-04-25 10:37:26 +0200

    Another iteration of layout.

--- js/scenario.js
+++ js/scenario.js
@@ -1,53 +1,133 @@
-// DOM element where the Timeline will be attached
-var container = document.getElementById("visualization");
+var scenario_info = {name:"my scenario 1",num_of_states:777,is_valid:true}
+var info_table_area = document.getElementById('scenario_info_area');
 
-// Create a DataSet (allows two way data-binding)
-var items = new vis.DataSet([
-  { id: 1, content: "item 1", start: "2014-04-20" },
-  { id: 2, content: "item 2", start: "2014-04-14" },
-  { id: 3, content: "item 3", start: "2014-04-18" },
-  { id: 4, content: "item 4", start: "2014-04-16", end: "2014-04-19" },
-  { id: 5, content: "item 5", start: "2014-04-25" },
-  { id: 6, content: "item 6", start: "2014-04-27", type: "point" },
-]);
-
-// Configuration for the Timeline
-var timeline_options = 
+var create_scenario_info_table = function (scenario) 
 	{
-		width: '100%',
-		height:'100%'
+		var info_table = document.createElement('table');
+			info_table.className = "pure-table pure-table-bordered";
+		var scenario_name_row = document.createElement('tr');
+		var num_of_states_row = document.createElement('tr');
+		var result_row = document.createElement('tr');
 		
-	};
+		var snc1 = document.createElement('td');
+		var snc2 = document.createElement('td');
+		var snc1_text = document.createTextNode('Scenario name:');
+		var snc2_text = document.createTextNode(scenario.name);
 
-// Create a Timeline
-var timeline = new vis.Timeline(container, items, timeline_options);
+			snc1.appendChild(snc1_text);
+			snc2.appendChild(snc2_text);
+			scenario_name_row.appendChild(snc1);
+			scenario_name_row.appendChild(snc2);
+		
+		var nosc1 = document.createElement('td');
+		var nosc2 = document.createElement('td');		
+		var nosc1_text = document.createTextNode('Number of states:');
+		var nosc2_text = document.createTextNode(scenario.num_of_states);
 
-// create an array with nodes
-var nodes = new vis.DataSet([
-  { id: 1, label: "Node 1" },
-  { id: 2, label: "Node 2" },
-  { id: 3, label: "Node 3" },
-  { id: 4, label: "Node 4" },
-  { id: 5, label: "Node 5" },
-]);
+			nosc1.appendChild(nosc1_text);
+			nosc2.appendChild(nosc2_text);
+			num_of_states_row.appendChild(nosc1);
+			num_of_states_row.appendChild(nosc2);
+			
+		var resc1 = document.createElement('td');
+		var resc2 = document.createElement('td');		
+		var resc1_text = document.createTextNode('Is scenario acceptable:');
+		var resc2_text;
+		
+		if (scenario.is_valid){
+			resc2_text = document.createTextNode("yes");
+			resc2.style.color = 'green';
+			resc2.style.fontWeight = '700';
+		}
+		else{
+			resc2_text = document.createTextNode("no");
+			resc2.style.color = 'red';
+			resc2.style.fontWeight = '700';
+			
+		}
 
-// create an array with edges
-var edges = new vis.DataSet([
-  { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-  { from: 2, to: 5 },
-  { from: 3, to: 3 },
-]);
+			resc1.appendChild(resc1_text);
+			resc2.appendChild(resc2_text);
+			result_row.appendChild(resc1);
+			result_row.appendChild(resc2);
+			
+			
+			info_table.appendChild(scenario_name_row);
+			info_table.appendChild(num_of_states_row);
+			info_table.appendChild(result_row);
 
-// create a network
-var container = document.getElementById("bigraph");
-var data = {
-  nodes: nodes,
-  edges: edges,
-};
-var network_options = 
+		return info_table;
+	}
+
+var info_table = create_scenario_info_table (scenario_info);
+	info_table_area.appendChild(info_table);
+
+var ui_map_table_area = document.getElementById('ui_map_area'),
+    ui_map_table = document.createElement('table');
+	ui_map_table.className = "pure-table pure-table-horizontal";
+	ui_map_table.id = "ui_map";
+var create_ui_map_table_header = function ()
 	{
-  	height: '100%',
-  };
-var network = new vis.Network(container, data, network_options);
+		var thead = document.createElement('thead');
+		var tr = document.createElement('tr');
+		tr.appendChild( document.createElement('th') );
+		tr.appendChild( document.createElement('th') );
+		tr.cells[0].appendChild( document.createTextNode("Unique object id") );
+		tr.cells[1].appendChild( document.createTextNode("Node's id") );
+		thead.appendChild(tr);
+		
+		return thead;
+	}
+var ui_map_thead = create_ui_map_table_header();
+var ui_map_tbody = document.createElement('tbody');
+
+	ui_map_table.appendChild(ui_map_thead);
+
+for (var i = 1; i < 30; i++) {
+    var tr = document.createElement('tr');
+
+    tr.appendChild( document.createElement('td') );
+    tr.appendChild( document.createElement('td') );
+
+    tr.cells[0].appendChild( document.createTextNode('Text1') )
+    tr.cells[1].appendChild( document.createTextNode('Text2') );
+
+    ui_map_tbody.appendChild(tr);
+}
+	ui_map_table.appendChild(ui_map_tbody);
+	ui_map_table_area.appendChild(ui_map_table);
+
+var create_sat_table_header = function ()
+	{
+		var thead = document.createElement('thead');
+		var tr = document.createElement('tr');
+		tr.appendChild( document.createElement('th') );
+		tr.appendChild( document.createElement('th') );
+		tr.cells[0].appendChild( document.createTextNode("Agent's id") );
+		tr.cells[1].appendChild( document.createTextNode("Time for the agent") );
+		thead.appendChild(tr);
+		
+		return thead;
+	}
+var sat_table_area = document.getElementById('sat_config_area'),
+	sat_table = document.createElement('table');
+	sat_table.className = "pure-table";
+	sat_table.id = "sat_config";
+var sat_thead = create_sat_table_header();
+var sat_tbody = document.createElement('tbody');
+
+	sat_table.appendChild(sat_thead);
+
+for (var i = 1; i < 7; i++) {
+    var tr = document.createElement('tr');
+
+    tr.appendChild( document.createElement('td') );
+    tr.appendChild( document.createElement('td') );
+
+    tr.cells[0].appendChild( document.createTextNode('Text1') )
+    tr.cells[1].appendChild( document.createTextNode('Text2') );
+
+    sat_tbody.appendChild(tr);
+}
+	sat_table.appendChild(sat_tbody);
+	sat_table_area.appendChild(sat_table);
