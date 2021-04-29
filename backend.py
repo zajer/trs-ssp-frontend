2215b2ad168af91562c6164e363454a2ab12696d
zajer <pikus3@list.pl>
2021-04-29 08:00:55 +0200

    Full backend API. Works but needs refining.

--- backend.py
+++ backend.py
@@ -1,24 +1,116 @@
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS, cross_origin
-import os
+import os, json, re
 
 app = Flask(__name__)
 root_dir = "data"
 
-@app.route('/chopstick/')
-@app.route('/chopstick/<int:chopstick_id>')
-def chopstick(chopstick_id=None):
-    if not chopstick_id:
-        message = jsonify(message='"chopstick_id" missing')
-        return make_response(message, 400)
-    chopstick = {
-        'id' : chopstick_id,
-        'color': 'bamboo',
-        'left_handed': True
+def _num_of_states (directory,regex):
+    files = os.listdir(directory)
+    result = 0
+    for file in files:
+        match = re.search(regex,file)
+        if not ( match is None ):
+            result = result + 1
+    return result
+def _load_json (relative_path_to_file):
+    path_to_file = os.path.join(root_dir,relative_path_to_file)
+    f = open(path_to_file)
+    data = json.load(f)
+    f.close()
+    return data
+def _scenario_overview_data(path_to_main_file):
+    f = open(path_to_main_file)
+    data = json.load(f)
+    f.close()
+    result = {        
+        'name': data['name'],
+        'num_of_states': _num_of_states(os.path.join(root_dir,data['directory']),data['states_regex']),
+        'is_valid': data['is_scenario_valid']
     }
-    return jsonify(chopstick)
+    return result
+def _get_timeline(scenario_main_file):
+    path_to_main_file = os.path.join(root_dir,scenario_main_file)
+    f = open(path_to_main_file)
+    data = json.load(f)
+    f.close()
+    result = { 
+            'groups_dataset': _load_json(data['groups_filename']),
+            'timeline_items_dataset': _load_json(data['timeline_filename'])
+        }
+    return result
+def _get_ith_state(directory,regex,i):
+    dir_absolute = os.path.join(root_dir,directory)
+    files = os.listdir(dir_absolute)
+    result = []
+    for file in files:
+        match = re.search(regex,file)
+        if not ( match is None ):
+            result.append(file)
+    res_filename = result[i] 
+    return _load_json(os.path.join(directory,res_filename))
+def _scenario_state_data(scenario_main_file,i):
+    path_to_main_file = os.path.join(root_dir,scenario_main_file)
+    f = open(path_to_main_file)
+    scenario = json.load(f)
+    f.close()
+    states_regex = scenario['states_regex']
+    states_dir = scenario['directory']
+    state_data = _get_ith_state(states_dir,states_regex,i)
+    print(state_data)
+    network_file = state_data['network_data_file']
+    result = {        
+        'network': _load_json(network_file),
+        'ui_map': state_data['ui_map'],
+        'sat_config': state_data['sat_config'],
+    }
+    return result
+def _does_scenario_exist(scenario_main_file):
+    path_to_file = os.path.join(root_dir,scenario_main_file)
+    if scenario_main_file == "" or not os.path.isfile(path_to_file):
+        return False
+    else:
+        return True
+def _does_state_exist(scenario_main_file,state_number):
+    scenario =_load_json(scenario_main_file)
+    num_of_states = _num_of_states(os.path.join(root_dir,scenario['directory']),scenario['states_regex'])
+    if num_of_states < state_number or state_number < 0:
+        return False
+    else:
+        return True  
+@app.route('/single/<string:scenario_main_file>/<int:state_number>')
+def single_in_moment(scenario_main_file="",state_number=-1):
+    scenario_validity_result = _does_scenario_exist(scenario_main_file)
+    if not scenario_validity_result:
+        message = jsonify(message='No such scenario')
+        return make_response(message, 400)
+    state_validity_result = _does_state_exist(scenario_main_file,state_number)
+    if not state_validity_result:
+        message = jsonify(message='This scenario does not have:'+str(state_number)+' states')
+        return make_response(message, 404)
+    return _scenario_state_data(scenario_main_file,state_number)
 
-def list_scenarios():
+@app.route('/single/<string:scenario_main_file>/timeline')
+def single_timeline(scenario_main_file=""):
+    scenario_validity_result = _does_scenario_exist(scenario_main_file)
+    if not scenario_validity_result:
+        message = jsonify(message='No such scenario')
+        return make_response(message, 400)
+    return _get_timeline(scenario_main_file)
+@app.route('/single/')
+@app.route('/single/<string:scenario_name>')
+def single_overview(scenario_name=""):
+    if scenario_name == "":
+        message = jsonify(message='Scenario name is missing')
+        return make_response(message, 400);
+    path_to_file = os.path.join(root_dir,scenario_name)
+    if not os.path.isfile(path_to_file):
+        message = jsonify(message='No such scenario.')
+        return make_response(message, 404)
+    else :
+        result = _scenario_overview_data(path_to_file)
+        return result
+def _list_scenarios():
     files = os.listdir(root_dir)
     keyword = 'trs-ssp-output.json'
     result = []
@@ -28,14 +120,12 @@ def list_scenarios():
             if keyword in file:
                 result.append(file)
     return result
-
-@app.route('/scenarios/')
+@app.route('/all/')
 @cross_origin(supports_credentials=True)
-def scenarios():
-    all_scenarios = list_scenarios()
+def all():
+    all_scenarios = _list_scenarios()
     resp = jsonify(all_scenarios)
     return resp
-
 if __name__ == '__main__':
     app.debug= True 
     app.run()
